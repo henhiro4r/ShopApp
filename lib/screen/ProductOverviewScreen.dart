cfc6ee55587b94ef1e85efdc7c1c44e41f929c28
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-05-13 18:02:08 +0700

    Filter ver 1

--- lib/screen/ProductOverviewScreen.dart
+++ lib/screen/ProductOverviewScreen.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/provider/ProductProvider.dart';
 import 'package:shopapp/widget/ProductGrid.dart';
+
+enum FilterOption {
+  Favorite,
+  All,
+}
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final products = Provider.of<ProductProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Product',
         ),
+        actions: <Widget>[
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            onSelected: (FilterOption val) {
+              if  (val == FilterOption.Favorite) {
+                products.showFavorite();
+              } else {
+                products.showAll();
+              }
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOption.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOption.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: ProductGrid(),
     );
