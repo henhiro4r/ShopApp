76bad88298b90170819e4dc638fa4be2177e4702
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-05-13 09:51:10 +0700

    Initial commits

---
+++ lib/adapter/ProductAdapter.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopapp/model/Product.dart';
+
+class ProductAdapter extends StatelessWidget {
+  final Product product;
+
+  ProductAdapter(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite_border),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
